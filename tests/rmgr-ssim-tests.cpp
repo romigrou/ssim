c33875d2ebb76f902a638d2940d325eb1b885cb7
Romain Bailly <romigrou@users.noreply.github.com>
2019-12-31 18:39:00 +0100

    Improved unit tests now also output performance metrics

--- tests/rmgr-ssim-tests.cpp
+++ tests/rmgr-ssim-tests.cpp
@@ -12,11 +12,18 @@ RMGR_WARNING_MSVC_DISABLE(4100) // unreferenced formal parameter
 RMGR_WARNING_POP()
 
 
+extern "C" int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
 namespace rmgr { namespace ssim
 {
     void gaussian_blur(Float* dest, ptrdiff_t destStride, const Float* srce, ptrdiff_t srceStride, int32_t width, int32_t height, const Float kernel[], int radius) RMGR_NOEXCEPT;
 
-    double sum_tile(uint32_t tileWidth, uint32_t tileHeight, uint32_t tileStride, double c1, double c2,
+    double sum_tile(uint32_t tileWidth, uint32_t tileHeight, uint32_t tileStride, Float c1, Float c2,
                     const Float* muATile, const Float* muBTile, const Float* sigmaA2Tile, const Float* sigmaB2Tile, const Float* sigmaABTile,
                     float* ssimTile, ptrdiff_t ssimStep, ptrdiff_t ssimStride) RMGR_NOEXCEPT;
 
@@ -34,14 +41,43 @@ enum Implementation
 };
 
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+#ifdef _WIN32
+    #include <windows.h>
+    #ifdef _MSC_VER
+        typedef unsigned __int64 uint64_t;
+    #endif
+
+    static inline uint64_t get_perf_counter() RMGR_NOEXCEPT
+    {
+        LARGE_INTEGER counter;
+        QueryPerformanceCounter(&counter);
+        return counter.QuadPart;
+    }
+
+    static uint64_t perf_counter_to_us(uint64_t counter) RMGR_NOEXCEPT
+    {
+        LARGE_INTEGER freq;
+        QueryPerformanceFrequency(&freq);
+        return (counter * 1000000) / freq.QuadPart;
+    }
+#else
+    #include <time.h>
+
+    static inline uint64_t get_perf_counter() RMGR_NOEXCEPT
+    {
+        timespec ts;
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+        return uint64_t(ts.tv_sec) * 1000000000u + ts.tv_nsec;
+    }
+
+    static uint64_t perf_counter_to_us(uint64_t counter) RMGR_NOEXCEPT
+    {
+        return counter / 1000u;
+    }
+#endif
 
 
-void test_compute_ssim(const char* imgPath, const char* refPath, Implementation impl, unsigned flags, const float expectedSSIMs[], float tolerance=1e-4f)
+void test_compute_ssim(uint64_t& elapsed, uint64_t& pixelCount, const char* imgPath, const char* refPath, Implementation impl, unsigned flags, const float expectedSSIMs[], float tolerance=1e-4f)
 {
     // Set implementation
     if (impl == IMPL_AUTO)
@@ -59,7 +95,7 @@ void test_compute_ssim(const char* imgPath, const char* refPath, Implementation 
 #if RMGR_ARCH_IS_X86_ANY
         ASSERT_NE(static_cast<rmgr::ssim::GaussianBlurFct>(NULL), rmgr::ssim::sse::g_gaussianBlurFct);
         rmgr::ssim::g_gaussianBlurFct = rmgr::ssim::sse::g_gaussianBlurFct;
-        rmgr::ssim::g_sumTileFct      = rmgr::ssim::sum_tile;
+        rmgr::ssim::g_sumTileFct      = rmgr::ssim::sse::g_sumTileFct;
 #else
         ASSERT_TRUE(false);
 #endif
@@ -92,9 +128,14 @@ void test_compute_ssim(const char* imgPath, const char* refPath, Implementation 
     float* ssimMap = new float[imgWidth * imgHeight];
     for (int channel=0; channel<refChannels; ++channel)
     {
+        uint64_t t1 = get_perf_counter();
         float ssim = rmgr::ssim::compute_ssim(refWidth, refHeight, refData+channel, refChannels, refWidth*refChannels, imgData+channel, imgChannels, imgWidth*imgChannels, ssimMap, 1, imgWidth, flags);
+        uint64_t t2 = get_perf_counter();
+        elapsed += t2 - t1;
         EXPECT_NEAR(expectedSSIMs[channel], ssim, tolerance);
     }
+
+    pixelCount += imgWidth * imgHeight * imgChannels;
     delete[] ssimMap;
 
     // Free images
@@ -109,12 +150,16 @@ static void test_einstein(Implementation impl, unsigned flags)
 //    const float ssims[] = {1.0f, 0.988f, 0.913f, 0.840f, 0.694f, 0.662f};
     const float ssims[] = {1.000000f, 0.987346f, 0.901217f, 0.839534f, 0.702192f, 0.669938f};
 
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/einstein.png",  RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+0);
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/meanshift.png", RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+1);
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/contrast.png",  RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+2);
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/impulse.png",   RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+3);
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/blur.png",      RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+4);
-    test_compute_ssim(RMGR_SSIM_TESTS_DIR "/images/jpg.png",       RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+5);
+    uint64_t elapsed    = 0;
+    uint64_t pixelCount = 0;
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/einstein.png",  RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+0);
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/meanshift.png", RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+1);
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/contrast.png",  RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+2);
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/impulse.png",   RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+3);
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/blur.png",      RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+4);
+    test_compute_ssim(elapsed, pixelCount, RMGR_SSIM_TESTS_DIR "/images/jpg.png",       RMGR_SSIM_TESTS_DIR "/images/einstein.png", impl, flags, ssims+5);
+    double throughput = double(pixelCount) / double(perf_counter_to_us(elapsed));
+    printf("  Throughput: %3.1f Mpix/s\n", throughput);
 }
 
 static void test_bbb(const char* stub, Implementation impl, unsigned flags, const float expectedSSIMs[][3])
@@ -122,13 +167,17 @@ static void test_bbb(const char* stub, Implementation impl, unsigned flags, cons
     char pngPath[256];
     snprintf(pngPath, sizeof(pngPath), "%s/images/%s.png", RMGR_SSIM_TESTS_DIR, stub);
 
+    uint64_t elapsed    = 0;
+    uint64_t pixelCount = 0;
     for (int jpgQuality=0; jpgQuality<=100; jpgQuality+=10)
     {
         char jpgPath[256];
         snprintf(jpgPath, sizeof(jpgPath), "%s/images/%s_%02d.jpg", RMGR_SSIM_TESTS_DIR, stub, jpgQuality);
         SCOPED_TRACE(jpgPath);
-        test_compute_ssim(jpgPath, pngPath, impl, flags, *expectedSSIMs++);
+        test_compute_ssim(elapsed, pixelCount, jpgPath, pngPath, impl, flags, *expectedSSIMs++);
     }
+    double throughput = double(pixelCount) / double(perf_counter_to_us(elapsed));
+    printf("  Throughput: %3.1f Mpix/s\n", throughput);
 }
 
 static void test_bbb360(Implementation impl, unsigned flags)
@@ -175,11 +224,17 @@ static void test_bbb1080(Implementation impl, unsigned flags)
 }
 
 
-#define TEST_IMPL(name, impl, IMPL)                                                     \
-    TEST(name, impl##_stack)        {test_##name(IMPL, 0);}                             \
-    TEST(name, impl##_heap)         {test_##name(IMPL, rmgr::ssim::FLAG_HEAP_BUFFERS);} \
-    TEST(name, impl##_stack_openmp) {test_##name(IMPL, rmgr::ssim::FLAG_OPENMP);}       \
-    TEST(name, impl##_heap_openmp)  {test_##name(IMPL, rmgr::ssim::FLAG_HEAP_BUFFERS|rmgr::ssim::FLAG_OPENMP);}
+#if RMGR_SSIM_USE_OPENMP
+    #define TEST_IMPL(name, impl, IMPL)                                                     \
+        TEST(name, impl##_stack)        {test_##name(IMPL, 0);}                             \
+        TEST(name, impl##_heap)         {test_##name(IMPL, rmgr::ssim::FLAG_HEAP_BUFFERS);} \
+        TEST(name, impl##_stack_openmp) {test_##name(IMPL, rmgr::ssim::FLAG_OPENMP);}       \
+        TEST(name, impl##_heap_openmp)  {test_##name(IMPL, rmgr::ssim::FLAG_HEAP_BUFFERS|rmgr::ssim::FLAG_OPENMP);}
+#else
+    #define TEST_IMPL(name, impl, IMPL)                                                     \
+        TEST(name, impl##_stack)        {test_##name(IMPL, 0);}                             \
+        TEST(name, impl##_heap)         {test_##name(IMPL, rmgr::ssim::FLAG_HEAP_BUFFERS);}
+#endif
 
 #define TEST_AUTO(name)     TEST_IMPL(name, auto,    IMPL_AUTO)
 #define TEST_GENERIC(name)  TEST_IMPL(name, generic, IMPL_GENERIC)
